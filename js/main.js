95808bfced73f04b81d2220a6ee163c027247766
Lena <fatmeow@gmail.com>
2017-12-21 16:01:08 -0500

    Add error class to both input fields

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var signIn = document.querySelector('button.signin'),
       modal = document.querySelector('.modal'),
-      close = document.querySelector('.close');
+      close = document.querySelector('.close'),
+      inputFields = modal.querySelectorAll('.getstarted > input'),
+      submit = modal.querySelector('button.submit');
 
   signIn.addEventListener('click', function() {
       modal.style.display = 'block';
@@ -10,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   close.addEventListener('click', function() {
       modal.style.display = 'none';
+  })
+
+  submit.addEventListener('click', function() {
+    inputFields.forEach(function(input) {
+      input.classList.add('error');
+    })
   })
 
 });
